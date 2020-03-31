ffa9324f9478fb98f16d48b3b7d37860f1335bbd
pierozkitwarozki <46139140+pierozkitwarozki@users.noreply.github.com>
2020-03-31 17:31:03 +0200

    'xxx'

---
+++ ContactList2.BLL/ContactListDataSource.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContactList2.BLL
+{
+    public static class ContactListDataSource
+    {
+        public static List<ContactList2.Models.Contact> GetContacts()
+        {
+            var contacts = new List<ContactList2.Models.Contact>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                contacts.Add(new ContactList2.Models.Contact(string.Format("My Contact {0}", i + 1),
+                                         "fake_email@gmail.com",
+                                         "+48 123 123 123"));
+            }
+
+            return contacts;
+        }
+    }
+}
